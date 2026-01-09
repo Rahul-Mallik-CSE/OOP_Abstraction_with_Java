8f4abe8e328e9bef483457d275e78813c8f680de
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-01-09 23:22:48 +0600

    Add abstract Animal class with instance variables and methods

--- src/App.java
+++ src/App.java
@@ -1,3 +1,27 @@
+abstract class Animal {
+    //instance variables
+    String name, color;
+    double weight;
+    //constructor
+    Animal(){
+
+    }
+
+    Animal(String name, String color, double weight){
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    //concrete method
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
